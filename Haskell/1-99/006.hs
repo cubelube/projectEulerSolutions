7a9a5aa910532830254205bf0e01444e4b274557
cubelube <114615759+cubelube@users.noreply.github.com>
2024-05-19 19:11:51 -0400

    tighten up the code a bit

--- Haskell/1-99/006.hs
+++ Haskell/1-99/006.hs
@@ -1,10 +1,4 @@
 -- https://projecteuler.net/problem=6
 
 main :: IO ()
-main = do
-    let squaredSum = sum [1..100] ** 2
-    let sumSquared = sum $ map (**2) [1..100]
-
-    let difference = squaredSum - sumSquared
-
-    print difference
+main = print ((sum [1..100] ** 2) - sum (map (**2) [1..100]))
